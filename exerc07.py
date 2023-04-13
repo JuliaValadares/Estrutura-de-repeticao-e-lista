3104f3698b82484ba0ffd60ead6f94000b36582d
Julia Valadares <ju.valadares12@gmail.com>
2023-04-13 11:40:00 -0300

    exerc07

---
+++ exerc07.py
@@ -0,0 +1,10 @@
+numeros = [1,2,3,4,5]
+soma = 0
+multi = numeros[0]
+print("numeros:", numeros)
+for i in numeros:
+    soma = soma + numeros [i-1]
+    multi = multi * numeros[i-1]
+    print("soma:", soma)
+    print("multiplicação:" , multi)
+
